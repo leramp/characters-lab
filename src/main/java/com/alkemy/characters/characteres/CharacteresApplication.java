62f2a85fc44ab45e0ec730ec1ea81802cd2b6c14
leramp <leandroramp@gmail.com>
2022-08-03 17:49:53 +0200

    Initial commit

---
+++ src/main/java/com/alkemy/characters/characteres/CharacteresApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.characters.characteres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacteresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacteresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/characters/characteres/CharacteresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.characters.characteres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacteresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
